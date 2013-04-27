ef7c25adf3ca0999811213f5e29ff8ae9be5d848
Johnathan David Froeming <johnathan@kodokuman.com>
2013-04-27 07:14:49 -0300

    Update MagedoxModel.java

--- src/com/kodokux/magedox/MagedoxModel.java
+++ src/com/kodokux/magedox/MagedoxModel.java
@@ -19,7 +19,7 @@ public class MagedoxModel implements Disposable {
 
     @Override
     public void dispose() {
-        System.out.println("MagedoxModel::dispose");
+        System.out.println("MagedoxModel::dispose 1");
     }
 
     public Project getProject () {
